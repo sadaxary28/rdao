6fdeb9b84fd344ec8d86abf1f8808f2b0edab352
Vladimir A. Ulitin <ulitin@infomaximum.com>
2017-05-19 14:04:42 +0300

    fix increment version

--- src/main/java/com/infomaximum/rocksdb/core/datasource/entitysource/EntitySource.java
+++ src/main/java/com/infomaximum/rocksdb/core/datasource/entitysource/EntitySource.java
@@ -1,11 +1,12 @@
 package com.infomaximum.rocksdb.core.datasource.entitysource;
 
+import java.io.Serializable;
 import java.util.Map;
 
 /**
  * Created by kris on 01.05.17.
  */
-public interface EntitySource {
+public interface EntitySource extends Serializable{
 
     public long getId();
 
