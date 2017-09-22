de180f771ae88da8bd57e94acf4ec945e82e86b1
mironov <mironov@mironov.ru>
2017-09-22 13:12:39 +0300

    Добавил performance-test.

--- src/test_performance/java/com/infomaximum/rocksdb/ReadTest.java
+++ src/test_performance/java/com/infomaximum/rocksdb/ReadTest.java
@@ -2,14 +2,16 @@ package com.infomaximum.rocksdb;
 
 import com.infomaximum.database.core.transaction.Transaction;
 import com.infomaximum.rocksdb.util.PerfomanceTest;
+import com.infomaximum.util.RandomUtil;
 import org.junit.Test;
 
 import java.util.Iterator;
+import java.util.UUID;
 
 public class ReadTest  extends BaseTest {
 
     @Test
-    public void iterateRecords1() throws Exception {
+    public void iterateRecords() throws Exception {
         final int recordCount = 10000;
 
         domainObjectSource.createEntity(RecordReadable.class);
@@ -26,6 +28,54 @@ public class ReadTest  extends BaseTest {
             Iterator<RecordReadable> i = domainObjectSource.iterator(RecordReadable.class);
             while (i.hasNext()) {
                 RecordReadable rec = i.next();
+            }
+        });
+    }
+
+    @Test
+    public void findAllByStringIndexedRecords() throws Exception {
+        final int recordCount = 100 * 1000;
+
+        domainObjectSource.createEntity(RecordIndexReadable.class);
+
+        final String fixedString = "some value";
+
+        Transaction transaction = domainObjectSource.getEngineTransaction().createTransaction();
+        for (int i = 0; i < recordCount; ++i) {
+            RecordIndexEditable rec = domainObjectSource.create(RecordIndexEditable.class);
+            rec.setString1((i % 100) == 0 ? fixedString : UUID.randomUUID().toString());
+            domainObjectSource.save(rec, transaction);
+        }
+        transaction.commit();
+
+        PerfomanceTest.test(1, step -> {
+            Iterator<RecordIndexEditable> i = domainObjectSource.findAll(RecordIndexEditable.class, RecordIndexEditable.FIELD_STRING_1, fixedString);
+            while (i.hasNext()) {
+                RecordIndexEditable rec = i.next();
+            }
+        });
+    }
+
+    @Test
+    public void findAllByLongIndexedRecords() throws Exception {
+        final int recordCount = 100 * 1000;
+
+        domainObjectSource.createEntity(RecordIndexReadable.class);
+
+        final long fixedLong = 500;
+
+        Transaction transaction = domainObjectSource.getEngineTransaction().createTransaction();
+        for (int i = 0; i < recordCount; ++i) {
+            RecordIndexEditable rec = domainObjectSource.create(RecordIndexEditable.class);
+            rec.setLong1((i % 100) == 0 ? fixedLong : RandomUtil.random.nextLong());
+            domainObjectSource.save(rec, transaction);
+        }
+        transaction.commit();
+
+        PerfomanceTest.test(1, step -> {
+            Iterator<RecordIndexEditable> i = domainObjectSource.findAll(RecordIndexEditable.class, RecordIndexEditable.FIELD_LONG_1, fixedLong);
+            while (i.hasNext()) {
+                RecordIndexEditable rec = i.next();
             }
         });
     }
--- src/test_performance/java/com/infomaximum/rocksdb/WriteTest.java
+++ src/test_performance/java/com/infomaximum/rocksdb/WriteTest.java
@@ -2,7 +2,10 @@ package com.infomaximum.rocksdb;
 
 import com.infomaximum.database.core.transaction.Transaction;
 import com.infomaximum.rocksdb.util.PerfomanceTest;
+import com.infomaximum.util.RandomUtil;
 import org.junit.Test;
+
+import java.util.UUID;
 
 public class WriteTest extends BaseTest {
 
@@ -27,6 +30,20 @@ public class WriteTest extends BaseTest {
         PerfomanceTest.test(100000, step-> {
             RecordEditable rec = domainObjectSource.create(RecordEditable.class);
             rec.setString1("some value");
+            domainObjectSource.save(rec, transaction);
+        });
+        transaction.commit();
+    }
+
+    @Test
+    public void createIndexedRecords() throws Exception {
+        domainObjectSource.createEntity(RecordIndexReadable.class);
+
+        Transaction transaction = domainObjectSource.getEngineTransaction().createTransaction();
+        PerfomanceTest.test(100000, step-> {
+            RecordIndexEditable rec = domainObjectSource.create(RecordIndexEditable.class);
+            rec.setString1(UUID.randomUUID().toString());
+            rec.setLong1(RandomUtil.random.nextLong());
             domainObjectSource.save(rec, transaction);
         });
         transaction.commit();
