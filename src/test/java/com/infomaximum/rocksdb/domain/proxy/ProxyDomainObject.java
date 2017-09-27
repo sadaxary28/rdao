3ed069284bf666b3e640ee6a8a5f82ddf2068027
Vladimir A. Ulitin <ulitin@infomaximum.com>
2017-09-27 17:30:41 +0300

    add junit test by proxy domain object

---
+++ src/test/java/com/infomaximum/rocksdb/domain/proxy/ProxyDomainObject.java
@@ -0,0 +1,85 @@
+package com.infomaximum.rocksdb.domain.proxy;
+
+import com.infomaximum.database.domainobject.DomainObject;
+import com.infomaximum.database.exeption.DataSourceDatabaseException;
+
+import java.util.Date;
+
+public class ProxyDomainObject extends DomainObject {
+
+    public ProxyDomainObject(long id) {
+        super(id);
+    }
+
+    @Override
+    public <T> T get(Class<T> type, String fieldName) {
+        try {
+            return super.get(type, fieldName);
+        } catch (DataSourceDatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected String getString(String fieldName) {
+        try {
+            return super.getString(fieldName);
+        } catch (DataSourceDatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected Integer getInteger(String fieldName) {
+        try {
+            return super.getInteger(fieldName);
+        } catch (DataSourceDatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected Long getLong(String fieldName) {
+        try {
+            return super.getLong(fieldName);
+        } catch (DataSourceDatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected Date getDate(String fieldName) {
+        try {
+            return super.getDate(fieldName);
+        } catch (DataSourceDatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected Boolean getBoolean(String fieldName) {
+        try {
+            return super.getBoolean(fieldName);
+        } catch (DataSourceDatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected byte[] getBytes(String fieldName) {
+        try {
+            return super.getBytes(fieldName);
+        } catch (DataSourceDatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected <T extends Enum> T getEnum(Class<T> enumClass, String fieldName) {
+        try {
+            return super.getEnum(enumClass, fieldName);
+        } catch (DataSourceDatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
