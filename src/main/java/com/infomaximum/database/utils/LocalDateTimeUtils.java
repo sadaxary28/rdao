b6a2fd22923d7b3a25e4375d720e971aa05d2b33
mironov <mironov@mironov.ru>
2018-10-27 11:04:00 +0300

    Добавил LocalDateTimeUtils.MAX, LocalDateTimeUtils.MIN

--- src/main/java/com/infomaximum/database/utils/LocalDateTimeUtils.java
+++ src/main/java/com/infomaximum/database/utils/LocalDateTimeUtils.java
@@ -8,12 +8,18 @@ public class LocalDateTimeUtils {
 
     private static final ZoneOffset OFFSET = ZoneOffset.UTC;
 
+    public static final LocalDateTime MAX = fromInstant(InstantUtils.MAX);
+    public static final LocalDateTime MIN =  fromInstant(InstantUtils.MIN);
+
     public static long toLong(LocalDateTime value) {
         return InstantUtils.toLong(value.toInstant(OFFSET));
     }
 
     public static LocalDateTime fromLong(long value) {
-        Instant instant = InstantUtils.fromLong(value);
+        return fromInstant(InstantUtils.fromLong(value));
+    }
+
+    private static LocalDateTime fromInstant(Instant instant) {
         return LocalDateTime.ofEpochSecond(instant.getEpochSecond(), instant.getNano(), OFFSET);
     }
 }
